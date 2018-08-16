9c062a1a4db96dc1aa90a5b53ddc2d6c4aad4608
Nathaniel Saul <nat@saulgill.com>
2018-08-15 23:09:00 -0700

    add logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -85,6 +85,8 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = 'logo.png'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.

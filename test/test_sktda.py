51ba9657b1bd7756b441f683b6d066787a31dbfd
Michael Catanzaro <catanzaromj@pm.me>
2024-02-24 09:24:29 -0500

    Update tests

--- test/test_sktda.py
+++ test/test_sktda.py
@@ -7,16 +7,22 @@ Should we expose each package at the sktda level?
 
 import sktda
 
+
 def test_ripser():
     assert "ripser" in dir(sktda)
-    assert callable(sktda.ripser)
+    assert callable(sktda.ripser.ripser)
+
 
 def test_persim():
-    assert "PersImage" in dir(sktda)
-    assert "heat" in dir(sktda)
-    assert "sliced_wasserstein" in dir(sktda)
-    assert "plot_diagrams" in dir(sktda)
+    assert "persim" in dir(sktda)
+    assert "PersImage" in dir(sktda.persim)
+    assert "heat" in dir(sktda.persim)
+    assert "sliced_wasserstein" in dir(sktda.persim)
+    assert "plot_diagrams" in dir(sktda.persim)
+    assert "landscapes" in dir(sktda.persim)
+
 
 def test_tadasets():
+    assert "tadasets" in dir(sktda)
     assert "dsphere" in dir(sktda.tadasets)
     assert "swiss_roll" in dir(sktda.tadasets)
